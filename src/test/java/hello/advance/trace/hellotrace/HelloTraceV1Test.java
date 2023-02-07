d5d5bd4b76e5da3c6e7a8b7d34088a549e9abcb2
cmong0516 <cmong0516@gmail.com>
2023-02-07 17:22:36 +0900

    2023.02.07

--- src/test/java/hello/advance/trace/hellotrace/HelloTraceV1Test.java
+++ src/test/java/hello/advance/trace/hellotrace/HelloTraceV1Test.java
@@ -1,5 +1,6 @@
 package hello.advance.trace.hellotrace;
 
+import hello.advance.trace.TraceId;
 import hello.advance.trace.TraceStatus;
 import org.junit.jupiter.api.Test;
 
@@ -7,6 +8,26 @@ public class HelloTraceV1Test {
     @Test
     void begin_end() {
         HelloTraceV1 trace = new HelloTraceV1();
+
+//        public TraceStatus begin(String message) {
+//            TraceId traceId = new TraceId();
+//            long startTimeMs = System.currentTimeMillis();
+//
+//            log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
+//
+//            return new TraceStatus(traceId, startTimeMs, message);
+//        }
+
+//        private static String addSpace(String prefix, int level) {
+//            StringBuilder sb = new StringBuilder();
+//            for (int i = 0; i < level; i++) {
+//                sb.append((i == level - 1) ? "|" + prefix : "|   ");
+//            }
+//            return sb.toString();
+//        }
+
+        // begin 에 String hello 를 입력하면 TraceId 생성하고 level 수만큼 StringBuilder 에 prefix 를 추가한다.
+
         TraceStatus status = trace.begin("hello");
         trace.end(status);
     }
